06c331f723a18acdde51bb80be2a47bc5bc0ab56
Radziu <RMazurczak@protonmail.com>
2024-12-15 03:07:21 +0100

    Stuff

--- Assets/ModelController.cs
+++ Assets/ModelController.cs
@@ -6,12 +6,16 @@ public class ModelController : NetworkBehaviour {
 
     [SerializeField] SkinnedMeshRenderer skinnedMeshRenderer;
     [SerializeField] List<MeshRenderer> meshRenderersList;
+    [SerializeField] List<BoxCollider> modelColliders;
 
     void Start() {
         if(IsOwner) {
             skinnedMeshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
             foreach(var renderer in meshRenderersList) {
                 renderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.ShadowsOnly;
+            }
+            foreach(var collider in modelColliders) {
+                collider.enabled = false;
             }
         } else {
             gameObject.SetActive(true);

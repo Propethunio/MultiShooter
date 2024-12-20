c0ebc91783000a7b4182808203fd5e2cd8940b2b
Radziu <RMazurczak@protonmail.com>
2024-12-20 20:28:00 +0100

    feat: code refactor and optymalization

--- Assets/Scripts/Crosshair.cs
+++ Assets/Scripts/Crosshair.cs
@@ -1,13 +1,15 @@
 ﻿/// <summary>
 /// This script belongs to cowsins™ as a part of the cowsins´ FPS Engine. All rights reserved. 
 /// </summary>
+
 using HEAVYART.TopDownShooter.Netcode;
 using System.Collections;
 using UnityEngine;
 
-namespace cowsins {
-
-    public class Crosshair : MonoBehaviour {
+namespace cowsins
+{
+    public class Crosshair : MonoBehaviour
+    {
         #region variables
 
         [Tooltip("Attach your PlayerMovement player "), SerializeField]
@@ -22,10 +24,11 @@ namespace cowsins {
         private Rigidbody rb;
 
         [Header("Variables")]
+        [SerializeField, Tooltip("If enabled, the crosshair will not be displayed when the game is paused.")]
+        private bool hideCrosshairOnPaused;
 
-        [SerializeField, Tooltip("If enabled, the crosshair will not be displayed when the game is paused.")] private bool hideCrosshairOnPaused;
-
-        [SerializeField, Tooltip("If enabled, the crosshair will not be displayed when the player is inspecting.")] private bool hideCrosshairOnInspecting;
+        [SerializeField, Tooltip("If enabled, the crosshair will not be displayed when the player is inspecting.")]
+        private bool hideCrosshairOnInspecting;
 
         [Tooltip(" How much space it takes from your screen"), SerializeField]
         private float size = 10f;
@@ -52,24 +55,25 @@ namespace cowsins {
 
         [SerializeField] private float resizeSpeed = 3f;
 
-        [HideInInspector]
-        public float spread;
+        [HideInInspector] public float spread;
 
-        [Header("Hitmarker")]
-
-        [SerializeField] private bool hitmarker;
+        [Header("Hitmarker")] [SerializeField] private bool hitmarker;
 
         [SerializeField] private GameObject hitmarkerObj;
 
         private bool isVisible = false;
 
-        public bool IsHidden { get { return isVisible; } }
+        public bool IsHidden
+        {
+            get { return isVisible; }
+        }
 
         private ShooterInputControls inputActions;
 
         #endregion
 
-        private void Awake() {
+        private void Awake()
+        {
             ResetCrosshair();
 
             playerStats = player.GetComponent<PlayerStats>();
@@ -79,41 +83,56 @@ namespace cowsins {
             crosshairShape = GetComponent<CrosshairShape>();
         }
 
-        private void Start() {
+        private void Start()
+        {
             inputActions = player.inputActions;
             StartCoroutine(ShowCrosshair());
         }
 
-        IEnumerator ShowCrosshair() {
-            while(true) {
-                if(GameManager.Instance.gameState == GameState.ActiveGame) {
+        IEnumerator ShowCrosshair()
+        {
+            while (true)
+            {
+                if (GameManager.Instance.gameState == GameState.ActiveGame)
+                {
                     break;
                 }
+
                 yield return null;
             }
+
             isVisible = true;
         }
 
-        private void Update() {
+        private void Update()
+        {
             // If we are shooting do not continue
-            if(inputActions.Player.Firing.IsPressed() && player.weaponController != null && player.weaponController.canShoot)
-                if(spread != defaultSpread) spread = Mathf.Lerp(spread, defaultSpread, resizeSpeed * Time.deltaTime / 10); // if this is not the current spread, fall back to the original one
+            if (inputActions.Player.Firing.IsPressed() && player.weaponController != null &&
+                player.weaponController.canShoot)
+                if (spread != defaultSpread)
+                    spread = Mathf.Lerp(spread, defaultSpread,
+                        resizeSpeed * Time.deltaTime /
+                        10); // if this is not the current spread, fall back to the original one
 
             // Manage different sizes
-            if(player.grounded) {
-                if(player.currentSpeed == player.runSpeed) Resize(runSpread);
-                else {
-                    if(player.currentSpeed == player.walkSpeed) {
-                        if(rb.linearVelocity.magnitude < .2f) Resize(defaultSpread);
-                        else Resize(walkSpread);
+            if (player.grounded)
+            {
+                if (player.currentSpeed == player.runSpeed) Resize(runSpread);
+                else
+                {
+                    if (player.currentSpeed == player.walkSpeed)
+                    {
+                        Resize(rb.linearVelocity.magnitude < .2f ? defaultSpread : walkSpread);
                     }
 
-                    if(player.currentSpeed == player.crouchSpeed) Resize(crouchSpread);
+                    if (player.currentSpeed == player.crouchSpeed) Resize(crouchSpread);
                 }
-            } else Resize(jumpSpread);
+            }
+            else Resize(jumpSpread);
         }
 
-        private void ResetCrosshair() {
+        private void ResetCrosshair()
+        {
             spread = defaultSpread;
             color = defaultColor;
             originalWidth = width;
@@ -123,44 +142,62 @@ namespace cowsins {
         /// Resize the crosshair to a new value.
         /// </summary>
         public void Resize(float newSize) => spread = Mathf.Lerp(spread, newSize, resizeSpeed * Time.deltaTime);
+
         /// <summary>
         /// Change color of the crosshair
         /// </summary>
-        public void SpotEnemy(bool condition) {
+        public void SpotEnemy(bool condition)
+        {
             color = (condition) ? enemySpottedColor : defaultColor;
-            width = (condition) ? Mathf.Lerp(width, enemySpottedWidth, resizeSpeed) : Mathf.Lerp(width, originalWidth, resizeSpeed);
+            width = (condition)
+                ? Mathf.Lerp(width, enemySpottedWidth, resizeSpeed)
+                : Mathf.Lerp(width, originalWidth, resizeSpeed);
         }
 
         /// <summary>
         /// Draw the crosshair as our UI
         /// </summary>
-        void OnGUI() {
-            if(playerStats.isDead
-                || weaponController.weapon != null && weaponController.isAiming && player.weaponController.removeCrosshairOnAiming || !isVisible) return;
-            Texture2D texture = new Texture2D(1, 1);
+        void OnGUI()
+        {
+            if (playerStats.isDead
+                || weaponController.weapon != null && weaponController.isAiming &&
+                player.weaponController.removeCrosshairOnAiming || !isVisible) return;
+            var texture = new Texture2D(1, 1);
             texture.SetPixel(0, 0, color);
             texture.wrapMode = TextureWrapMode.Repeat;
             texture.Apply();
 
 
-            if(crosshairShape.parts.downPart) GUI.DrawTexture(new Rect(Screen.width / 2 - width / 2, (Screen.height / 2 - size / 2) + spread / 2, width, size), texture);
+            if (crosshairShape.parts.downPart)
+                GUI.DrawTexture(
+                    new Rect(Screen.width / 2 - width / 2, (Screen.height / 2 - size / 2) + spread / 2, width, size),
+                    texture);
 
-            if(crosshairShape.parts.topPart) GUI.DrawTexture(new Rect(Screen.width / 2 - width / 2, (Screen.height / 2 - size / 2) - spread / 2, width, size), texture);
+            if (crosshairShape.parts.topPart)
+                GUI.DrawTexture(
+                    new Rect(Screen.width / 2 - width / 2, (Screen.height / 2 - size / 2) - spread / 2, width, size),
+                    texture);
 
-            if(crosshairShape.parts.rightPart) GUI.DrawTexture(new Rect((Screen.width / 2 - size / 2) + spread / 2, Screen.height / 2 - width / 2, size, width), texture);
+            if (crosshairShape.parts.rightPart)
+                GUI.DrawTexture(
+                    new Rect((Screen.width / 2 - size / 2) + spread / 2, Screen.height / 2 - width / 2, size, width),
+                    texture);
 
-            if(crosshairShape.parts.leftPart) GUI.DrawTexture(new Rect((Screen.width / 2 - size / 2) - spread / 2, Screen.height / 2 - width / 2, size, width), texture);
+            if (crosshairShape.parts.leftPart)
+                GUI.DrawTexture(
+                    new Rect((Screen.width / 2 - size / 2) - spread / 2, Screen.height / 2 - width / 2, size, width),
+                    texture);
 
-            if(crosshairShape.parts.center) {
-                float radius = Mathf.Min(width, size) / 2;
-                Vector2 center = new Vector2(Screen.width / 2, Screen.height / 2);
-                Rect circleRect = new Rect(center.x - radius, center.y - radius, radius * 2, radius * 2);
+            if (!crosshairShape.parts.center) return;
+            var radius = Mathf.Min(width, size) / 2;
+            var center = new Vector2(Screen.width / 2, Screen.height / 2);
+            var circleRect = new Rect(center.x - radius, center.y - radius, radius * 2, radius * 2);
 
-                GUI.DrawTexture(circleRect, texture);
-            }
+            GUI.DrawTexture(circleRect, texture);
         }
 
-        public void SetVisibility(bool visible) {
+        public void SetVisibility(bool visible)
+        {
             isVisible = visible;
         }
     }
